fb2ba4ac2de3e85b822b2f70d81fb7d5df2220fd
Matthew Charles Baker <fimbaz@gmail.com>
2021-12-22 15:42:58 -0800

    looking ugly but better

--- algofs/create.bash
+++ algofs/create.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo -u algorand -E goal app create --creator $HOT_WALLET_KEY --global-byteslices 61 --global-ints 3 --local-byteslices 0 --local-ints 0 --approval-prog $1 --clear-prog clear.teal
+sudo -u algorand -E goal app create --creator $HOT_WALLET_KEY --global-byteslices 60 --global-ints 4 --local-byteslices 0 --local-ints 0 --approval-prog $1 --clear-prog clear.teal
 
